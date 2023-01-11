eca3fa089cb3a8e57c977f6c842deab2e53d2d75
Kirill Kozhevnikov <kkojevnikov1993@gmail.com>
2023-01-11 23:56:08 +0700

    change1

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,30 @@
-import java.awt.*;
 
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Здравствуйте!");
         System.out.println("Ваш баланс:");
-        int account = 150;
-        int replenishment = 1100;
-        boolean balance = true;
 
-        int bonus;
-        if (balance) {
-            bonus = 1;
+        int balance = 150;
+        int replenesmint = 6500;
+
+        int amount;
+        if (replenesmint >= 1000) {
+            amount = 1;
         } else {
-            bonus = 0;}
-        int money = account + replenishment + bonus;
+            amount = 0;
+        }
 
-        System.out.println(money);
-     }
+        int bonus = replenesmint / 100 * amount;
+
+        System.out.println(balance + replenesmint);
+
+        System.out.println("Ваш бонус:");
+
+        System.out.println(bonus);
+
+
+        System.out.println("Спасибо, что доверяете нам!");
+    }
 }
+
